b1d0862c89e17a587cdf4ce1725d6bf8be1f08ef
BogdanDiaconescu <dbogdi@gmail.com>
2022-07-17 10:30:25 +0300

    Initial commit

---
+++ src/main/java/eu/accesa/internshipfactory/InternshipFactoryApplication.java
@@ -0,0 +1,13 @@
+package eu.accesa.internshipfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipFactoryApplication.class, args);
+	}
+
+}
